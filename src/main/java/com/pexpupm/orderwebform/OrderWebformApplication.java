f6fc87d1b2b77ebc490abca7cfc5aebbb7841131
Sergio Arroutbi Braojos <sergio.arroutbi.braojos@ericsson.com>
2018-10-20 11:19:08 +0200

    Base commit

---
+++ src/main/java/com/pexpupm/orderwebform/OrderWebformApplication.java
@@ -0,0 +1,12 @@
+package com.pexpupm.orderwebform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderWebformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderWebformApplication.class, args);
+	}
+}
