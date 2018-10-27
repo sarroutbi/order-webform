e7e7fee171938e6a72cd436b55a5362706581271
Sergio Arroutbi Braojos <sergio.arroutbi.braojos@ericsson.com>
2018-10-27 18:49:28 +0200

    Add page to show delivery and fix all deliveries

--- src/main/java/com/pexpupm/orderwebform/DeliveryController.java
+++ src/main/java/com/pexpupm/orderwebform/DeliveryController.java
@@ -2,11 +2,14 @@ package com.pexpupm.orderwebform;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class DeliveryController {
@@ -49,22 +52,35 @@ public class DeliveryController {
 	}
 
 	@RequestMapping("/delivery/new")
-	public String nuevoAnuncio(Model model, Delivery delivery) {
+	public String nuevoAnuncio(Model model, Delivery delivery, @RequestParam String[] elements) {
 		System.out.println("New Delivery!!! Name:" + delivery.getName());
-		for (Element elem : delivery.getElements()) {
-			System.out.println("  * Elem:" + elem.getName());
+		for (String elem : elements) {
+			Element element = new Element(elem);
+			element.setDelivery(delivery);
+			delivery.getElements().add(element);
+			System.out.println(" * Elem:" + elem);
 		}
 		deliveries.add(delivery);
 		deliveryRepository.save(delivery);
 		model.addAttribute("delivery", delivery);
 		return "delivery_correctly_stored";
 	}
-//
-//	@RequestMapping("/delivery/{delivery_id}")
-//	public String showDelivery(Model model, @PathVariable int delivery_id) {
-//		Delivery delivery = deliveries.get(delivery_id - 1);
-//		model.addAttribute("delivery", delivery);
-//		return "show_delivery";
-//	}
+
+	@RequestMapping("/delivery/{delivery_id}")
+	public String showDelivery(Model model, @PathVariable Long delivery_id) {
+		Optional<Delivery> delivery = deliveryRepository.findById(delivery_id);
+		if (delivery.isPresent()) {
+			model.addAttribute("delivery", delivery.get());
+			List<Element> elem_list = new ArrayList<Element>();
+			for(Element elem: elementRepository.findByDeliveryName(delivery.get().getName())) {
+				elem_list.add(elem);
+			}
+			model.addAttribute("elements", elem_list);
+			return "show_delivery";
+		}
+		model.addAttribute("delivery_id", delivery_id);
+		return "delivery_id_not_found";
+		//Delivery delivery = deliveries.get(delivery_id - 1);
+	}
 }
 
