121a54a3c3d663eb067a331a598abaa6cf01fef7
Sergio Arroutbi Braojos <sergio.arroutbi.braojos@ericsson.com>
2018-10-27 19:02:27 +0200

    Show empty message on empty persisted deliver list

--- src/main/java/com/pexpupm/orderwebform/DatabaseUsage.java
+++ src/main/java/com/pexpupm/orderwebform/DatabaseUsage.java
@@ -16,33 +16,36 @@ public class DatabaseUsage implements CommandLineRunner {
 	@Override
 	public void run(String ...args) throws Exception {
 		System.out.println("**** Starting ****");
-		Delivery delivery1 = new Delivery("Delivery1");
-		Delivery delivery2 = new Delivery("Delivery2");
 
-		Element coffee = new Element("Coffee");
-		Element milk = new Element("Milk");
-		milk.setDelivery(delivery1);
-		coffee.setDelivery(delivery1);
-		delivery1.getElements().add(coffee);
-		delivery1.getElements().add(milk);
-		deliveryRepository.save(delivery1);
+		// This code allows to insert some persisted elements on the database
+		// for testing purposes before main logic is executed
+//		Delivery delivery1 = new Delivery("Delivery1");
+//		Delivery delivery2 = new Delivery("Delivery2");
+//
+//		Element coffee = new Element("Coffee");
+//		Element milk = new Element("Milk");
+//		milk.setDelivery(delivery1);
+//		coffee.setDelivery(delivery1);
+//		delivery1.getElements().add(coffee);
+//		delivery1.getElements().add(milk);
+//		deliveryRepository.save(delivery1);
+//
+//		Element water = new Element("Water");
+//		Element cola = new Element("Cola");
+//		cola.setDelivery(delivery2);
+//		water.setDelivery(delivery2);
+//		delivery2.getElements().add(water);
+//		delivery2.getElements().add(cola);
+//		deliveryRepository.save(delivery2);
+//
+//		for (Delivery delivery : deliveryRepository.findAll()) {
+//			System.out.println("- Delivery:" + delivery.getName());
+//			for(Element elem: elementRepository.findByDeliveryName(delivery.getName())) {
+//				System.out.println("  * Elem:" + elem.getName());
+//			}
+//		}
 
-		Element water = new Element("Water");
-		Element cola = new Element("Cola");
-		cola.setDelivery(delivery2);
-		water.setDelivery(delivery2);
-		delivery2.getElements().add(water);
-		delivery2.getElements().add(cola);
-		deliveryRepository.save(delivery2);
-
-		for (Delivery delivery : deliveryRepository.findAll()) {
-			System.out.println("- Delivery:" + delivery.getName());
-			for(Element elem: elementRepository.findByDeliveryName(delivery.getName())) {
-				System.out.println("  * Elem:" + elem.getName());
-			}
-		}
-
-		// This code shows how to delete complete deliveries or elements
+		// This code shows how to delete complete deliveries or elements for a delivery
 //		deliveryRepository.delete(deliveryRepository.findByName("Delivery1"));
 //		Element elemToDelete = elementRepository.findByNameAndDeliveryName("Water", "Delivery2");
 //		elementRepository.deleteById(elemToDelete.getId());
